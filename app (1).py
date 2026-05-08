a55e76d07e190be580c884a0cf6a39e99570850e
SidraNaveed19 <sidranaveed192004@gmail.com>
2026-05-08 12:26:35 +0100

    Update app (1).py

--- app (1).py
+++ app (1).py
@@ -16,18 +16,18 @@ st.write("Enter patient details below:")
 
 # Inputs
 age = st.number_input("Age", 1, 120)
-sex = st.selectbox("Sex (0 = Female, 1 = Male)", [0, 1])
-cp = st.selectbox("Chest Pain Type (0–3)", [0, 1, 2, 3])
+sex = st.number_input("Sex (0 = Female, 1 = Male)")
+cp = st.number_input("Chest Pain Type (0–3)")
 trestbps = st.number_input("Resting Blood Pressure", 80, 200)
 chol = st.number_input("Cholesterol Level", 100, 600)
-fbs = st.selectbox("Fasting Blood Sugar > 120 (1 = Yes, 0 = No)", [0, 1])
-restecg = st.selectbox("Rest ECG (0–2)", [0, 1, 2])
+fbs = st.number_input("Fasting Blood Sugar ")
+restecg = st.number_input("Rest ECG (0–2)", [0, 1, 2])
 thalach = st.number_input("Max Heart Rate Achieved", 60, 220)
-exang = st.selectbox("Exercise Induced Angina (1 = Yes, 0 = No)", [0, 1])
+exang = st.number_input("Exercise Induced Angina ")
 oldpeak = st.number_input("Oldpeak (ST depression)", 0.0, 10.0)
-slope = st.selectbox("Slope (0–2)", [0, 1, 2])
-ca = st.selectbox("Number of Major Vessels (0–3)", [0, 1, 2, 3])
-thal = st.selectbox("Thal (1 = Normal, 2 = Fixed Defect, 3 = Reversible)", [1, 2, 3])
+slope = st.number_input"Slope (0–2)")
+ca = st.number_input("Number of Major Vessels (0–3)")
+thal = st.number_input("Thal (1 = Normal, 2 = Fixed Defect, 3 = Reversible)")
 
 # Prepare input
 import pandas as pd
